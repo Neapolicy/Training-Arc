609758abc48e574b690000330c9d150fe2376674
Neapolicy <matthewlin559@gmail.com>
2023-11-05 18:45:14 -0500

    set up the basics of card generation for old maid specifically, I believe with poker it should still be the same

---
+++ src/Player.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+public class Player {
+    private ArrayList<String> deck = new ArrayList<String>();
+    public Player() // player for normal poker
+    {
+        for (int i = 0; i  < 5; i++) {
+            Card c = new Card();
+            String val = c.getValue();
+            deck.add(val);
+            System.out.println(c.getColor() + " " + c.getSuit() + " " + c.getValue());
+        }
+        System.out.print(deck);
+    }
+
+    public Player(boolean b) // player for old maid
+    {
+        for (int i = 0; i  < 5; i++) {
+            Card c = new Card();
+            String val = c.getValue();
+            deck.add(val);
+        }
+        addJoker();
+    }
+
+    private void addJoker()
+    {
+        Card c = new Card(true);
+        String val = c.getValue();
+        deck.add(val);
+    }
+
+    public ArrayList<String> getDeck()
+    {
+        return deck;
+    }
+
+    public void setDeck(ArrayList<String> deck)
+    {
+        this.deck = deck;
+    }
+}
